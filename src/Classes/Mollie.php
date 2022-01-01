cec816bbbf7a5179c21409ae95f174563d7d0284
Robin van Maasakker <robin@qubiqx.com>
2022-01-01 20:02:13 +0100

    Update

--- src/Classes/Mollie.php
+++ src/Classes/Mollie.php
@@ -71,33 +71,6 @@ class Mollie
         }
     }
 
-//    public static function getPaymentMethods($siteId = null, $cache = true)
-//    {
-//        $site = Sites::get($siteId);
-//
-//        if (! Customsetting::get('mollie_connected', $site['id'])) {
-//            return;
-//        }
-//
-//        if (! $cache) {
-//            Cache::forget('mollie-payment-methods-' . $site['id']);
-//        }
-//
-//        $result = Cache::remember('mollie-payment-methods-' . $site['id'], 60 * 60 * 24, function () use ($site) {
-//            config(['mollie.key' => Customsetting::get('mollie_api_key', $site['id'])]);
-//
-//            $result = \Mollie\Laravel\Facades\Mollie::api()->methods()->allActive()->getArrayCopy();
-//            foreach ($result as $paymentMethod) {
-//                $paymentMethod->active = Customsetting::get('mollie_payment_method_' . $paymentMethod->id, $site['id'], 0) ? true : false;
-//                $paymentMethod->costs = Customsetting::get('mollie_payment_method_costs_' . $paymentMethod->id, $site['id'], 0);
-//            }
-//
-//            return $result;
-//        });
-//
-//        return $result;
-//    }
-
     public static function startTransaction(OrderPayment $orderPayment)
     {
         $orderPayment->psp = 'mollie';
@@ -114,7 +87,7 @@ class Mollie
             ]),
             'redirectUrl' => route('qcommerce.frontend.checkout.complete') . '?orderId=' . $orderPayment->order->hash . '&paymentId=' . $orderPayment->hash,
             'webhookUrl' => route('qcommerce.frontend.checkout.exchange'),
-            'method' => str_replace('mollie_', '', $orderPayment->psp_payment_method_id),
+            'method' => $orderPayment->paymentMethod->psp_id,
             'metadata' => [
                 'order_id' => Translation::get('payment-description', 'payments', 'Order #:orderId:', 'text', [
                     'orderId' => $orderPayment->order->id,
@@ -125,7 +98,10 @@ class Mollie
         $orderPayment->psp_id = $payment->id;
         $orderPayment->save();
 
-        return $payment;
+        return [
+            'transaction' => $payment,
+            'redirectUrl' => $payment->getCheckoutUrl(),
+        ];
     }
 
     public static function getOrderStatus(OrderPayment $orderPayment)
