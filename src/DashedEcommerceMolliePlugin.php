ae46395c29c2c0764c803fb25e4f623df10e6a2a
Robinvm <Robinvm@users.noreply.github.com>
2023-11-08 14:59:11 +0000

    Fix styling

--- src/DashedEcommerceMolliePlugin.php
+++ src/DashedEcommerceMolliePlugin.php
@@ -2,9 +2,9 @@
 
 namespace Dashed\DashedEcommerceMollie;
 
-use Filament\Panel;
-use Filament\Contracts\Plugin;
 use Dashed\DashedEcommerceMollie\Filament\Pages\Settings\MollieSettingsPage;
+use Filament\Contracts\Plugin;
+use Filament\Panel;
 
 class DashedEcommerceMolliePlugin implements Plugin
 {
